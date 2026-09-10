deef0b5968a37ad063dfa7cecd90499c24dfb430
SilentDev <gyurci.dev@gmail.com>
2026-09-10 19:04:18 +0200

    pass syntax update,fix

--- parser.py
+++ parser.py
@@ -121,10 +121,13 @@ class Parser:
             )
         if token.type is TokenType.RETURN:
             return self.parse_return()
+        
         if token.type is TokenType.PASS:
+         self.advance()
+        while not self.at(TokenType.NEWLINE) and not self.at(TokenType.EOF):
             self.advance()
-            self.expect(TokenType.NEWLINE, "a `pass` után sorvége kell")
-            return PassStmt()
+        self.match(TokenType.NEWLINE)
+        return PassStmt()
         if token.type is TokenType.AWAIT:
             self.advance()
             expression = self.parse_expression()
